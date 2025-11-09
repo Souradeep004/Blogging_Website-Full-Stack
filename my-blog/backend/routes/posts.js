cb1c39b52ab30d134474441ea44380651daf52ec
Ujjansh Sundram <ujjanshsundram2006@gmail.com>
2025-11-10 03:16:42 +0530

    updated files

--- my-blog/backend/routes/posts.js
+++ my-blog/backend/routes/posts.js
@@ -11,13 +11,17 @@ const {
 
 const { protect } = require('../middleware/auth');
 
+// Allow open post creation in development or when ALLOW_OPEN_POSTS=true
+const allowOpenPosts = process.env.ALLOW_OPEN_POSTS === 'true' || process.env.NODE_ENV === 'development';
+const openOrProtect = allowOpenPosts ? (req, res, next) => next() : protect;
+
 // Public routes
 router.get('/', getPosts);
 router.get('/search', searchPosts);
 router.get('/:id', getPost);
 
 // Protected routes (require authentication)
-router.post('/', protect, createPost);
+router.post('/', openOrProtect, createPost);
 router.put('/:id', protect, updatePost);
 router.delete('/:id', protect, deletePost);
 
